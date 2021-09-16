1d404442419bf179406fff8a3042890c0a4b4ea8
markusp21 <89973049+markusp21@users.noreply.github.com>
2021-09-16 11:34:30 +0000

    Update tiletraveller.py

--- tiletraveller.py
+++ tiletraveller.py
@@ -9,3 +9,23 @@
         #Check if input matches one of available paths
     #Victory condition
         #User has reached last box, (3,1) print victory and stop running
+
+#Algorithm
+    #Start game 1.1
+        #User options: What the user can do, which movements the user can choose from and make a decision. 
+        #Input: The program takes in the input from the user (n, e, w or s) and checks if the input matches the available paths the user is given beforehand to confirm it. 
+        #Repeat: The program puts the user in the new location, after the program has checked if the input matches the available paths and repeats the command until the final destination is reached.
+    #Victory 3.1
+
+def location():
+    print()
+
+def where_you_can_travel():
+    print()
+
+def input1():
+    print()
+
+def confirm_if_valid():
+    print()
+
