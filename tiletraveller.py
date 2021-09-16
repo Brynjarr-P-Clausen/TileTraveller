46b85128d2d7ad81233d465d19aa3da42aff586b
Brynjarr-P-Clausen <89972353+Brynjarr-P-Clausen@users.noreply.github.com>
2021-09-16 10:23:10 +0000

    Add files via upload

---
+++ tiletraveller.py
@@ -0,0 +1,11 @@
+#Make functions:
+    #Start the game
+        #Place user in first box (1,1)
+    #Where you can travel
+        #Check directions to travel and print available paths
+    #Input
+        #Take input from user
+    #Confirm if valid
+        #Check if input matches one of available paths
+    #Victory condition
+        #User has reached last box, (3,1) print victory and stop running
