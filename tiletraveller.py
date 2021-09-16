61b2061d590764fd30be6477397b638b4ad8dbee
unknown <danieloa21@ru.is>
2021-09-16 13:32:51 +0000

    TileTraveller4

--- tiletraveller.py
+++ tiletraveller.py
@@ -17,77 +17,80 @@
         #Repeat: The program puts the user in the new location, after the program has checked if the input matches the available paths and repeats the command until the final destination is reached.
     #Victory 3.1
 
-tile1 = 1.1 
-tile2 = 1.2
-tile3 = 1.3
-tile4 = 2.1
-tile5 = 2.2
-tile6 = 2.3
-tile7 = 3.1
-tile8 = 3.2
-tile9 = 3.3
+
 valid_direction = 0
+x, y = 1, 1
+    
 
 
 def main():
     # 1. tell user where he can go
+    while x != 3 and y != 1: 
+        where_you_can_travel(x, y)
+    
     # 2. ask user where he wants to go
+        input1(valid_direction)
     # 3. verify input is valid
     # 4a. if valid: move
     # 4b. else: punsih user go agane
+    if x == 3 and y == 1:
+        print("Victory!")
     # 5. check if destination reached while not tile 7
-    location = tile1
-    x, y = 1, 1
-    y += 1 #norður
-    y -= 1 #suður
-    x += 1 #austur
-    x -= 1 #vestur
+    
     pass
+    
 
 
 def location(l):
     
     return l
 
-def where_you_can_travel():
-    if location == tile1:
+def where_you_can_travel(x, y):
+    if x == 1 and y == 1:
         print("You can travel: (N)orth. ")
         valid_direction = "n".casefold()
-    elif location == tile2:
+    elif x == 1 and y == 2:
         print("You can travel: (N)orth or (E)ast or (S)outh.")
         valid_direction = "n".casefold(), "e".casefold(), "s".casefold()
-    elif location == tile3:
+    elif x == 1 and y == 3:
         print("You can travel: (E)ast or (S)outh.")
         valid_direction =  "e".casefold(), "s".casefold()
-    elif location == tile4:
+    elif x == 2 and y == 1:
         print("You can travel: (N)orth.")
         valid_direction = "n".casefold()
-    elif location == tile5:
+    elif x == 2 and y == 2:
         print("You can travel: (S)outh or (W)est.")
         valid_direction = "w".casefold(), "s".casefold()        
-    elif location == tile6:
+    elif x == 2 and y == 3:
         print("You can travel: (E)ast or (W)est.")
         valid_direction = "w".casefold(), "e".casefold(), 
-    elif location == tile7:
+    elif x == 3 and y == 1:
         print("Victory!")
-    elif location == tile8:
+    elif x == 3 and y == 2:
         print("You can travel: (N)orth or (S)outh.")
         valid_direction = "n".casefold(),  "s".casefold()
-    elif location == tile9:
+    elif x == 3 and y == 3:
         print("You can travel: (S)outh or (W)est.") 
         valid_direction = "w".casefold(), "s".casefold()
     return(valid_direction)
 
 def input1():
-    input("Direction: ")
-    return(input1)
-
-def confirm_if_valid():
-    if input1 == valid_direction:
-        #go to next location
-        print()
+    way = input("Direction: ".casefold())
+    if way == valid_direction:
+        if way == "n":
+            y + 1
+        elif way == "s":
+            y - 1
+        elif way == "e":
+            x + 1
+        elif way == "w":
+            x - 1
     else:
-        print("Invalid Direction")
+        print("Invalid Direction:")
+
+    return way
+
+
 
 
 main()
